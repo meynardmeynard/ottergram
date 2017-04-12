ff8a8e9ab807c2ac593e34c129bae4f302e141eb
Jericho Mendoza <jerichomm6@gmail.com>
2017-04-12 11:41:25 +0800

    challenge-js-done

--- scripts/main.js
+++ scripts/main.js
@@ -21,9 +21,19 @@ function titleFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-title');
 }
 
+function randImage(){
+  a = ["https://cdn.shopify.com/s/files/1/0032/7882/products/tacocat_art.png?v=1447870796",
+       "https://smhttp-ssl-5891.nexcesscdn.net/media/catalog/product/cache/1/image/1800x/6b9ffbf72458f4fd2d3cb995d92e8889/t/a/tacocat_newthumb.png",
+       "http://www.tshirtlaundry.com/assets/images/photos/Tacocat3-5-2013-2.jpg",
+       "https://s-media-cache-ak0.pinimg.com/236x/a0/2e/a9/a02ea959c1ab35b4ebcd81564b902555.jpg",
+       "https://i.ytimg.com/vi/oQvxASWxZZo/hqdefault.jpg",
+       "http://www.dimmak.com/wp-content/uploads/2013/10/post-14725-Taco-cat-backwards-is-still-ta-mSjr.jpeg"];
+  return a[Math.floor(Math.random()*a.length)]
+}
+
 function setDetailsFromThumb(thumbnail) {
   'use strict';
-  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+  setDetails(randImage(), titleFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
